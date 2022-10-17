f4de27340906af8dfe40da63997b79dab4d9e8bd
unknown <ikwuonwuvictor@gmail.com>
2022-10-17 22:05:27 +0100

    Did Level 1 and part of level 2, stopped at line 85(console.log(numbers))

---
+++ Week 1/myScript.js
@@ -0,0 +1,85 @@
+let challenge = '30 Days Of JavaScript';
+// console.log(challenge);
+// console.log(challenge.length);
+
+// console.log(challenge.toUpperCase())
+
+// console.log(challenge.toLowerCase())
+
+// console.log(challenge.substr(2, 4))
+// console.log(challenge.slice(2))
+
+// console.log(challenge.includes("Script"))
+
+// console.log(challenge.split(' '))
+
+let techCompanies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+
+// console.log(techCompanies.split(','))
+
+// console.log(challenge.replace('JavaScript', 'Python'))
+
+// console.log(challenge.charAt(15))
+// console.log(challenge.charCodeAt('J'))
+
+// console.log(challenge.indexOf('a'))
+// console.log(challenge.lastIndexOf('a'))
+
+let sentence = 'You cannot end a sentence with because because because is a conjunction';
+
+// console.log(sentence.indexOf('because'))
+// console.log(sentence.lastIndexOf('because'))
+
+// console.log(sentence.search('because'))
+
+let space = ' 30 Days Of JavaScript '
+
+// console.log(space.trim())
+
+// console.log(challenge.startsWith('3'))
+// console.log(challenge.endsWith('t'))
+
+// console.log(challenge.match(/a/gi))
+
+let days = '30 Days of'
+
+// console.log(days.concat(" Javascript"))
+
+let task = days.concat(" Javascript")
+
+// console.log(task)
+
+/* Level 2 */
+
+// console.log(`The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.`)
+
+// console.log(`"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."`)
+
+// let num = '10'
+
+// console.log('10' === 10);
+
+// let num1 = +num;
+// console.log(num1)
+
+// console.log(parseFloat('9.8'));
+
+// let programmingLanguages = "python and jargon";
+
+// console.log(programmingLanguages.includes('on'))
+
+// let sentence2 = "I hope this course is not full of jargon. Check if jargon is in the sentence."
+// console.log(sentence2.includes('jargon'));
+
+// console.log(Math.floor(Math.random() * 101))
+// console.log(Math.floor(Math.random() * 51) + 50)
+console.log(Math.floor(Math.random() * 256))
+
+let numbers = `1 1 1 1 1 
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125`
+
+console.log(numbers)
+
