b2e4cab86c296a43ceec51e1e39a11766149045a
unknown <ikwuonwuvictor@gmail.com>
2022-11-10 15:49:33 +0100

    week 4

---
+++ Week 4/app.js
@@ -0,0 +1,85 @@
+// let age = window.prompt("Enter your age")
+
+// if (age < 18) {
+//     console.log(`You will be eligible to drive in ${18 - age} years time`)
+// } else {
+//     console.log("You are eligible to drive")
+// }
+
+// let myAge;
+// let yourAge;
+
+// if (myAge > yourAge) {
+//     console.log(`I am older than you`)
+// } else {
+//     console.log("You are older")
+// };
+
+// let a;
+// let b;
+
+// if (a > b) {
+//     console.log(`${a} is greater than ${b}`)
+// } else {
+//     console.log(`${b} is greater than ${a}`)
+// }
+
+// let num
+
+// if (num / 2 && num > 0) {
+//     console.log(`${num} is an even number`)
+// } else {
+//     console.log(`${num} is an odd number`)
+// }
+
+let score = 81;
+
+// switch (score) {
+//     case (score > 88 && score <= 100):
+//         console.log('You got an A')
+//         break;
+//     case score > 69 && score <= 89 :
+//         console.log('You got an B')
+//         break;
+//     case score > 59 && score <= 69 :
+//         console.log('You got an C')
+//         break;
+//     case score > 49 && score <= 59 :
+//         console.log('You got an D')
+//         break;
+//     case score >= 0 && score <= 49 :
+//         console.log('You got an F')
+//         break;
+
+//     default:
+//         console.log('Your exam paper got lost')
+//         break;
+// }
+
+if (score > 88 && score <= 100) {
+    
+    console.log('You got an A')
+
+} else if(score > 69 && score <= 89) {
+
+    console.log('You got an B')
+
+} else if(score > 59 && score <= 69) {
+
+    console.log('You got an C')
+    
+} else if(score > 49 && score <= 59) {
+
+    console.log('You got an D')
+    
+} else if(score >= 0 && score <= 49) {
+
+    console.log('You got an F')
+    
+} else {
+    console.log('Your exam paper got lost')
+} 
+
+
+
+
